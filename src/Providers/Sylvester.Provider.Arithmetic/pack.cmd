07ed79bc5c37248cb82abc093199187f104fcced
Allister Beharry <allister.beharry@gmail.com>
2019-06-09 11:33:26 -0400

    Add pack script.

--- src/Providers/Sylvester.Provider.Arithmetic/pack.cmd
+++ src/Providers/Sylvester.Provider.Arithmetic/pack.cmd
@@ -1,2 +1,2 @@
 @echo off
-.paket\paket.exe pack src\Sylvester.Provider.Arithmetic.Runtime\paket.template --version %1
+.paket\paket pack --template .\src\Sylvester.Provider.Arithmetic.Runtime\paket.template --version %1 .\lib\
