da908c58b0c28c4005d074ba80e7c6f42bd90bd9
Allister Beharry <allister.beharry@gmail.com>
2019-06-15 16:22:18 -0400

    Remove HList global operators.

--- src/Base/Sylvester.Arithmetic.Collections/HList.fs
+++ src/Base/Sylvester.Arithmetic.Collections/HList.fs
@@ -7,19 +7,15 @@ module HList =
 
     type HList = interface end
 
-    type HNil = HNil with
-        interface HList
-        static member inline (|*|) (f, HNil) = f $ HNil
-        static member inline (!?)(HNil) = _true
-        static member inline (!+)(HNil) = zero
-       
     type HCons<'a, 'b when 'b :> HList>  = HCons of 'a * 'b with
         interface HList
         static member inline (|*|) (f, HCons(x, xs)) = f $ HCons(x, xs) 
         static member inline (!?) (HCons(x, xs)) = _false
-        static member inline (!+)(HNil) = zero
+        static member inline (^+^) (HCons(x, xs), y) = HCons(HCons(x, xs), y)
+        static member inline (^+^) (y, HCons(x, xs)) = HCons(y, HCons(x, xs))
+        static member inline (^++^) (x, y) = (HAppend $ x) <| y
         static member inline (!+)(HCons(x, xs)) = (!+ xs) + one
-
+          
         static member inline (|@|) (HCons(x, _), _:N1<_0>) = x
         static member inline (|@|) (HCons(_, HCons(y, _)), _:N1<_1>) = y
         static member inline (|@|) (HCons(_, HCons(_, HCons(y, _))), _:N1<_2>) = y
@@ -31,13 +27,20 @@ module HList =
         static member inline (|@|) (HCons(_, HCons(_, HCons(_, HCons(_, HCons(_, HCons(_, HCons(_, HCons(_, HCons(y, _))))))))), _:N1<_8>) = y
         static member inline (|@|) (HCons(_, HCons(_, HCons(_, HCons(_, HCons(_, HCons(_, HCons(_, HCons(_, HCons(_, HCons(y, _)))))))))), _:N1<_9>) = y
          
-    type HAppend = HAppend with
+    and HNil = HNil with
+        interface HList
+        static member inline (|*|) (f, HNil) = f $ HNil
+        static member inline (!?)(HNil) = _true
+        static member inline (!+)(HNil) = zero
+        static member inline (^+^) (x, HNil) = HCons(x, HNil)
+       
+    and HAppend = HAppend with
         static member ($) (HAppend, HNil) = id
         static member inline ($) (HAppend, HCons(x, xs)) = fun list ->
             HCons (x, (HAppend |*| xs) list)
 
-    let inline (^+^) head tail = HCons(head, tail)
-    let inline (^++^) x y = (HAppend $ x) <| y
+
+
         
     
 
