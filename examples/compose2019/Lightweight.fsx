0aa437b70ba55bac79bdd9c115cd8e530b97b526
allisterb <allister.beharry@gmail.com>
2019-06-25 14:32:03 -0400

    Add more examples and data.

--- examples/compose2019/Lightweight.fsx
+++ examples/compose2019/Lightweight.fsx
@@ -9,47 +9,7 @@ open Sylvester.Arithmetic
 open Sylvester.Arithmetic.N10
 open Sylvester.Tensors
 
-let v100 = vnew hundred 5.0 
+let vf = new VecF<"C:\\Projects\\Sylvester\\examples\\data\\iris.data", false, 1>()
 
-let v9000 = vrand (nine * thousand)
-
-let v200= Vec<200>.Rand
-
-let v200b = Vec<200>.Rand
-
-let s1 = v200 + v200b
-
-//v200 + v45f //Type error
-
-let p1 = v200 * v200b
-
-let pv1 = v200 * scalar 4.0f
-
-//let p2 = v100 * v200 //type error
-
-let c1 = v100 +!> v200
-
-let c2 = v100 +!< v200
-
-let c3 =  (v200b +!< v200) <?> (44, "foo") 
-
-let m4200 = Mat<4, 200>.Rand
-
-let m44 = Mat<4, 4>.Rand
-
-let mnotsquare = Mat<1000, 700>.Rand
-
-let inline foo a b =
-    check(a +!> b)
-
-let c4 = foo v9000 v100
-//let c4 = foo v100 v9000 //Doesn't satisfy constraint
-
-
-let m44det = mdet m44
-
-let m44inv = minv m44
-
-//let mnsinv = minv mnotsquare //Doesn't satisfy matrix function constraint
-
+vf.Dim0
 
