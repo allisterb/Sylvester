a46b267add1939c6222fcb2a9ed5a529fd6d665d
Allister Beharry <allister.beharry@gmail.com>
2019-11-12 21:16:09 -0400

    Fix bug in ND provider. Add reference to ND provider to Library project.

--- src/Fabrics/Sylvester.Fabric.Keras/Library.fs
+++ src/Fabrics/Sylvester.Fabric.Keras/Library.fs
@@ -1,5 +1,11 @@
 ﻿namespace Sylvester.Fabric.Keras
 
+
 module Say =
+    open Numpy
+    open Sylvester.Fabric.Keras
+    open Sylvester.Arithmetic
     let hello name =
+        let v = new ND<2,4>()
+        v
         printfn "Hello %s" name
