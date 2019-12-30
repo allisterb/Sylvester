7a9b0d5fb393127a7af2cf62ab1bc41faeaa30ab
Allister Beharry <allister.beharry@gmail.com>
2019-12-30 01:01:51 -0400

    Update Sylvester.Arithmetic package to 0.2.

--- examples/compose2019/N10.fsx
+++ examples/compose2019/N10.fsx
@@ -1,8 +1,13 @@
 ﻿#r ".\\..\\..\\src\\Providers\\Sylvester.Provider.Arithmetic\\src\\Sylvester.Provider.Arithmetic.Runtime\\bin\\Release\\net45\\Sylvester.Provider.Arithmetic.Runtime.dll"
+#r ".\\..\\..\\src\\Base\\Sylvester.Collections\\bin\\Debug\\net45\\Sylvester.Collections.dll"
 
 
 open Sylvester.Arithmetic
 open Sylvester.Arithmetic.N10
+open Sylvester.Collections
 
-let j = new n<45>()
+let j = new dim<45>()
 
+let x = j + dim<60>.create()
+
+let v = varray<dim<2>, int>
