a32b9b689bb772cdcbf915796a09c19dfd5e3313
Allister Beharry <allister.beharry@gmail.com>
2020-01-14 21:43:41 -0400

    Add AbstractGraph implementation.

---
+++ src/Fabrics/tf/Sylvester.tf.Graphs/GraphDefn.fs
@@ -0,0 +1,17 @@
+﻿namespace Sylvester.tf
+
+open System.IO
+open Sylvester
+
+open Google.Protobuf
+open Google.Protobuf.Collections
+open Tensorflow
+
+open Satsuma
+
+type GraphDefn (stream:Stream) = 
+    inherit Api()
+    let pbdefn = GraphDef.Parser.ParseFrom(CodedInputStream.CreateWithLimits(stream, 256 * 1024 * 1024, 100))
+    let graph = new CustomGraph()
+    let ids = Seq.iter (fun n -> graph.Nodes.) (defn.Node)
+    //Seq.iter (fun n:NodeDef -> n.) def.
