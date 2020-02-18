f1552e03a755d27579779503934759618b2886ce
Allister Beharry <allister.beharry@gmail.com>
2020-02-18 13:50:26 -0400

    left_assoc rule works.

--- src/Lang/Sylvester.Prover/Formula.fs
+++ src/Lang/Sylvester.Prover/Formula.fs
@@ -11,11 +11,13 @@ open FSharp.Quotations.ExprShape
 type Formula<'t, 'u>([<ReflectedDefinition(true)>] expr: Expr<'t -> 'u>) =
     let (v, e) = expandReflectedDefinitionParam expr
     member val Obj = v
+    [<ReflectedDefinition>]
     member val Apply = v :?> ('t -> 'u)
     member val Expr = e
     member val Src = decompile e
+    member x.Members = (x, x.Apply)
     override x.ToString() = x.Src
-    static member (|->) (l: Formula<'t, 'u>, r: 't) = l.Apply r
+
     static member (<=>) (lhs:Formula<'t, 'u>, rhs:Formula<'t, 'u>) = sequal lhs.Expr rhs.Expr
     static member (<=>) (lhs:Expr, rhs:Formula<'t, 'u>) = sequal lhs rhs.Expr
     static member (<=>) (lhs:Formula<'t, 'u>, rhs:Expr) = sequal lhs.Expr rhs
@@ -29,7 +31,7 @@ type Predicate<'t> = Formula<'t, bool>
 [<AutoOpen>]
 module Formula =
     [<ReflectedDefinition>] 
-    let Value (x:'t) = Value(fun () -> x)
+    let value (x:'t) = Value(fun () -> x)
     
     let src (f:Formula<'t, 'u>) = decompile f.Expr
     
@@ -74,30 +76,15 @@ module FormulaPatterns =
         | Call(None, method, Sequence(l, r)::[]) when method.Name = "Sum" -> Some (l, r)
         | _ -> None
 
+    let (|Commute|_|) (A, B) =
+        match A,B with
+        // x + y, y + x
+        | Lambda(_, BinaryOp(a1, a2)), Lambda(_, BinaryOp(b1, b2)) when sequal2 a1 a2 b2 b1 -> Some B
+        | _ -> None
+
     let (|Assoc|_|) (A, B) =
         match A,B with
         // x + y + z, x + (y + z)
         | Lambda(_,BinaryOp(BinaryOp(a1, a2), a3)), Lambda(_, BinaryOp(b1, BinaryOp(b2, b3))) when (sequal3 a1 a2 a3 b1 b2 b3) -> Some B 
         | Lambda(_, BinaryOp(a1, BinaryOp(a2, a3))), Lambda(_, BinaryOp(BinaryOp(b1, b2), b3)) when (sequal3 a1 a2 a3 b1 b2 b3)-> Some B
         | _ -> None
-
-
-
-[<AutoOpen>]
-module ArithmeticRules =
-    
-    open ArithmeticPatterns
-    
-    let rec reduceConstantIntOperands (expr:Expr) =
-        match expr with
-        | Add(Add(l, Int32 lval), Int32 rval) -> let s = Expr.Value(lval + rval) in <@@ %%l + %%s @@>
-        | _ -> traverse expr reduceConstantIntOperands
-    
-    
-    let rec replaceSum (expr:Expr) =
-        match expr with
-        | Sum (l, r) -> expr
-        | _ -> traverse expr replaceSum
-
-    let g = <@@ replaceSum @@>
-    let gg = g.Substitute(fun v -> if v.Name = "x" then Some (Expr.Var(Var.Global("x", v.Type))) else None)
--- src/Lang/Sylvester.Prover/Proof.fs
+++ src/Lang/Sylvester.Prover/Proof.fs
@@ -8,24 +8,23 @@ open FSharp.Quotations.Patterns
 open FSharp.Quotations.DerivedPatterns
 open FSharp.Quotations.ExprShape
 
+open FormulaPatterns
 
-type ProofSystem =  ProofSystem of (Expr->Expr->Expr option) * (Expr->Expr list)
+type Axioms = (Expr * Expr->Expr option)
+type Rules = (Expr->Expr) list 
+type ProofCalculus =  ProofCalculus of Axioms * Rules
 
-module IntegerAxioms =
-    open ArithmeticPatterns
-    
-    let integer_axioms = 
+module Proof =   
+    let integerAxioms = 
         function
         | Assoc x -> Some x
         | _ -> None
-    
-    let axiomatic_equality (l:Formula<)
+
     let rec reduce_constants  =
         function
         | Add(Int32 l, Int32 r) -> Expr.Value(l + r)
         | Subtract(Int32 l, Int32 r) -> Expr.Value(l - r)
         | Multiply(Int32 l, Int32 r) -> Expr.Value(l * r)
-        | Add(Add(l, Int32 lval), Int32 rval) -> let s = Expr.Value(lval + rval) in <@@ %%l + %%s @@>
         | expr -> traverse expr reduce_constants
 
     let rec right_assoc =
@@ -38,4 +37,4 @@ module IntegerAxioms =
         | Add(a1, Add(a2, a3)) -> <@@ (%%a1 + %%a2) + %%a3@@>
         | expr -> traverse expr left_assoc
 
-
+    let IntegerCalculus = ProofCalculus(integerAxioms, [reduce_constants; right_assoc; left_assoc])
