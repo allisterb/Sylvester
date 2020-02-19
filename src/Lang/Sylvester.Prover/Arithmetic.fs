d2d85410aa5a5227a4e663838e7eae39ca064d2e
Allister Beharry <allister.beharry@gmail.com>
2020-02-19 05:50:38 -0400

    Add more printing proof status.

--- src/Lang/Sylvester.Prover/Arithmetic.fs
+++ src/Lang/Sylvester.Prover/Arithmetic.fs
@@ -23,7 +23,7 @@ module Arithmetic =
         | Multiply(Int32 l, Int32 r) -> Expr.Value(l * r)
         | expr -> traverse expr reduce_constants
 
-    let reduce_constants_a_b = Rule("Reduce constants in A and B.", fun (a,b) -> reduce_constants a, reduce_constants b)
+    let reduce_constants_a_b = Rule("Reduce constants in A and B", fun (a,b) -> reduce_constants a, reduce_constants b)
     
     let rec right_assoc =
         function
@@ -35,12 +35,12 @@ module Arithmetic =
         | Add(a1, Add(a2, a3)) -> <@@ (%%a1 + %%a2) + %%a3@@>
         | expr -> traverse expr left_assoc
 
-    let left_assoc_a = Rule("A is left associative.", fun (a,b) -> left_assoc a, b)
+    let left_assoc_a = Rule("A is left associative", fun (a,b) -> left_assoc a, b)
     
     let left_assoc_b = Rule("B is left associative", fun (a, b) -> a, left_assoc b)
 
-    let right_assoc_a = Rule("A is right associative.", fun (a, b) -> right_assoc a, b)
+    let right_assoc_a = Rule("A is right associative", fun (a, b) -> right_assoc a, b)
     
-    let right_assoc_b = Rule("B is right associative.", fun (a, b) -> a, right_assoc b)
+    let right_assoc_b = Rule("B is right associative", fun (a, b) -> a, right_assoc b)
 
     let Arithmetic = ProofSystem(integer_axioms, [reduce_constants_a_b; left_assoc_a; left_assoc_b; right_assoc_a; right_assoc_b])
--- src/Lang/Sylvester.Prover/Proof.fs
+++ src/Lang/Sylvester.Prover/Proof.fs
@@ -19,20 +19,37 @@ type ProofSystem(axioms: Axioms, rules: Rules) =
 type Proof(system: ProofSystem, a:Expr,  b:Expr,  steps: Rule list) =
     let ruleNames = system.Rules |> List.map (fun r -> r.Name)
     let stepNames = steps |> List.map (fun r -> r.Name)
+    do printfn "Proof of %s <=> %s." (decompile a) (decompile b)
     let mutable astate, bstate = (a, b)
     let mutable stepCount = 0
-    do printfn "Proof"
     do while stepCount < steps.Length do
         if not(List.contains stepNames.[stepCount] ruleNames) then
-            printfn "Step %i (%s) is not part of the rules of the proof system." stepCount stepNames.[stepCount]
-            stepCount <- steps.Length
+            failwithf "Step %i (%s) is not part of the rules of the proof system." stepCount stepNames.[stepCount]
         else
             let (_a, _b) = steps.[stepCount].Apply (astate, bstate)
-            printfn "(%s, %s) -> (%s, %s)" (decompile astate) (decompile bstate) (decompile _a) (decompile _b)
+            printfn "%i. %s: (%s, %s) -> (%s, %s)" stepCount stepNames.[stepCount] (decompile astate) (decompile bstate) (decompile _a) (decompile _b)
             astate <- _a
             bstate <- _b
-            stepCount <- stepCount + 1
-type Theorem<'t, 'u>(lhs:Formula<'t, 'u>, rhs:Formula<'t, 'u>, proof:Proof) = class end
+            if system |- (astate, bstate) then 
+                printfn "Proof complete." 
+                stepCount <- steps.Length
+            else
+                printfn "Proof incomplete."
+                stepCount <- stepCount + 1
+    
+    member val Complete = system |- (astate, bstate)
+    member val A = a
+    member val B = b
+    member val System = system
+    member val Steps = steps
+    static member (|-) ((proof:Proof), (a, b)) = proof.A = a && proof.B = b && proof.Complete
+
+type Theorem<'t, 'u>(a:Formula<'t, 'u>, b:Formula<'t, 'u>, proof:Proof) = 
+    do if not (proof.A = a.Expr && proof.B = b.Expr) then failwithf "The provided proof is not a proof of %s<=>%s" (a.Src) (b.Src)
+    do if not (proof.Complete) then failwithf "The provided proof of %s<=>%s is not complete." (a.Src) (b.Src)
+    member val A = a
+    member val B = b
+    member val Proof = proof
    
 module Proof =   
     let thm (lhs:Formula<_, _>) (rhs:Formula<_, _>) (proof) = Theorem(lhs, rhs, proof)
