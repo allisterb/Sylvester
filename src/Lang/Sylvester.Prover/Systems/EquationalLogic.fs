c4454dd0bdedfcbb73d16514b5454131b0f00336
Allister Beharry <allister.beharry@gmail.com>
2020-02-25 08:44:52 -0400

    Add remaining axioms to E.

--- src/Lang/Sylvester.Prover/Systems/EquationalLogic.fs
+++ src/Lang/Sylvester.Prover/Systems/EquationalLogic.fs
@@ -5,18 +5,20 @@ open FSharp.Quotations.ExprShape
 
 open Sylvester
 
-/// Formalizes the system of equational logic (E) used by Sylph
-/// http://www.cs.cornell.edu/home/gries/Logic/Equational.html
+/// Formalizes the system of equational logic used by Sylph
+/// Based on E: http://www.cs.cornell.edu/home/gries/Logic/Equational.html
+/// The main difference is that since we only have to deal with symbolic equality (not mathematical equality)
+//  we can drop the restriction that a substitution must only replace variables in an expression.
 module EquationalLogic =
     (* Patterns *)
 
     /// Expr A is transformed by a substitution S into B.
     let (|Substitution|_|) =
         function
-        | (S, A, B) when sequal (S A) B -> Some (A, B)
+        | (S, A, B) when sequal (S A) B -> Some (S, A, B)
         | _ -> None
 
-    /// A pair (Expr A, Expr B) is transformed by a recursive substitution S into (C, D).
+    /// A pair (Expr A, Expr B) is transformed by a substitution S into (C, D).
     let (|Substitution2|_|) =
         function
         | (S, A, B, C,D) when let (c, d) = S(A, B) in sequal2 c d C D -> Some (S, A, B, C, D)
@@ -28,9 +30,15 @@ module EquationalLogic =
         | ShapeLambda(avar, abody) -> Expr.Lambda(avar, abody) |> Some
         | _ -> None
 
+    /// A formula substitution from one formula to another of the same type.
+    let (|FormulaSubstitution|_|) =
+        function
+        | Substitution(_, Formula(ShapeLambda(avar, abody)), Formula(ShapeLambda(bvar, bbody))) 
+                                when vequal avar bvar -> Expr.Lambda(avar, abody) |> Some
+        | _ -> None
 
     /// A theorem has the form Formula A == Formula B where the pair (A, B) is transformed by 
-    //  recursive substitution into (C, D)
+    //  substitution into (C, D)
     let (|Theorem|_|) =
         function
         | Substitution2(_, Formula(ShapeLambda(avar, abody)), Formula (ShapeLambda(bvar, bbody)), 
@@ -56,18 +64,18 @@ module EquationalLogic =
         | _ -> None
 
     /// If A == B is a theorem and B == C is a theorem then we can infer A == C.
-    //let (|Transitivity|_|) =
-    //    function
-    //    | Theorem(body1, body2), Theorem(body3, body4), Theorem(body5, body6) 
-    //         when sequal body1 body5 && sequal body2 body3 && sequal body4 body5 && sequal body2 body6  -> Some true
-    //    | _ -> None
+    let (|Transitivity|_|) =
+        function
+        | Theorem(body1, body2), Theorem(body3, body4), Theorem(body5, body6) 
+             when sequal body1 body5 && sequal body2 body3 && sequal body4 body5 && sequal body2 body6  -> Some true
+        | _ -> None
  
     /// If A == B is a theorem then we can infer S(A) == S(B).
-    //let (|Leibniz|_|) =
-    //    function
-    //    | (S, Theorem(labody, rabody), Theorem(lbbody, rbbody)) 
-    //        when sequal2 lbbody rbbody (traverse labody S) (traverse rabody S) -> Some true
-    //    | _ -> None
+    let (|Leibniz|_|) =
+        function
+        | (S, Theorem(labody, rabody), Theorem(lbbody, rbbody)) 
+            when sequal2 lbbody rbbody (S labody) (S rabody) -> Some true
+        | _ -> None
 
     //let E =
     //    function
