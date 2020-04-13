7c78695f7623aa91e0c48c8fe43e2345aa92a8c4
Allister Beharry <allister.beharry@gmail.com>
2020-04-13 13:53:42 -0400

    Test JS method compiles.

--- examples/js/Include.fsx
+++ examples/js/Include.fsx
@@ -4,8 +4,10 @@
 
 #r ".\\..\\..\\ext\\FunScript\\src\\main\\FunScript\\bin\\Debug\\netstandard2.0\\FunScript.Util.dll"
 #r ".\\..\\..\\ext\\FunScript\\src\\main\\FunScript\\bin\\Debug\\netstandard2.0\\FunScript.Interop.NETStandard.dll"
-#r ".\\..\\..\\ext\\FunScript\\src\\main\\FunScript.Bindings.Base\\bin\\Debug\\netstandard2.0\\FunScript.Bindings.Base.dll"
 #r ".\\..\\..\\ext\\FunScript\\src\\main\\FunScript\\bin\\Debug\\netstandard2.0\\FunScript.dll"
+#r ".\\..\\..\\ext\\FunScript\\src\\main\\FunScript.Bindings.Base\\bin\\Debug\\netstandard2.0\\FunScript.Bindings.Base.dll"
+#r ".\\..\\..\\ext\\FunScript\\src\\extra\\FunScript.D3\\bin\\Debug\\netstandard2.0\\FunScript.D3.dll"
+
 
 open System
 open FunScript
@@ -16,9 +18,16 @@ open FunScript.Bindings
 [<AutoOpen>]
 module Foo =
     // Create a function that will be compiled into JavaScript...
-    let htmlCanvasTS(f)=
+    let htmlCanvasTS()=
         let canvas = Globals.document.getElementsByTagName_canvas().[0]
-        Globals.alert(f)
+        canvas.width <- 1000.
+        canvas.height <- 800.
+        let ctx = canvas.getContext_2d()
+        ctx.fillStyle <- "rgb(200,0,0)"
+        ctx.fillRect (10., 10., 55., 50.);
+        ctx.fillStyle <- "rgba(0, 0, 200, 0.5)"
+        ctx.fillRect (30., 30., 55., 50.)
 
 let js =  Compiler.Compile(<@ htmlCanvasTS @>, noReturn=true)
-<@ htmlCanvasTS("foo")@>
+printf "%s" js
+//<@ htmlCanvasTS()@>
--- tests/Sylvester.Tests.FunScript/Tests.fs
+++ tests/Sylvester.Tests.FunScript/Tests.fs
@@ -4,21 +4,24 @@ open System
 open Xunit
 
 open FunScript
-open FunScript.Bindings
+open FunScript.Bindings.D3
 
-[<ReflectedDefinition>]
+
 module Foo =   
     // Create a function that will be compiled into JavaScript...
+    let d = []
+    [<JS>]
     let htmlCanvasTS()=
-        let canvas = Globals.document.getElementsByTagName_canvas().[0]
-        canvas.width <- 1000.
-        canvas.height <- 800.
-        let ctx = canvas.getContext_2d()
+        //let canvas = Globals.document.getElementsByTagName_canvas().[0]
+        //canvas.width <- 1000.
+        //canvas.height <- 800.
+        //let ctx = canvas.getContext_2d()
     
-        ctx.fillStyle <- "rgb(200,0,0)"
-        ctx.fillRect (10., 10., 55., 50.);
-        ctx.fillStyle <- "rgba(0, 0, 200, 0.5)"
-        ctx.fillRect (30., 30., 55., 50.)
+        //ctx.fillStyle <- "rgb(200,0,0)"
+        //ctx.fillRect (10., 10., 55., 50.);
+        //ctx.fillStyle <- "rgba(0, 0, 200, 0.5)"
+        //ctx.fillRect (30., 30., 55., 50.)
+        d3.selectAll().attr("ff", "gg").text("ff")
 
 
 [<Fact>]
@@ -31,4 +34,5 @@ let ``My test`` () =
             // This argument tells the compiler not to wrap 
             // the result in a return statement
             noReturn=true)
-    Assert.NotNull htmlCanvasCode
+    let s =  htmlCanvasCode
+    Assert.NotNull s
