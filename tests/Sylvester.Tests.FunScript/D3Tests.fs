b58ccdae2a89e3afad608018f14778ee1c1bea72
Allister Beharry <allister.beharry@gmail.com>
2020-04-17 06:15:53 -0400

    Add D3Tests.

---
+++ tests/Sylvester.Tests.FunScript/D3Tests.fs
@@ -0,0 +1,19 @@
+namespace Sylvester.Tests.FunScript
+
+open System
+open Xunit
+
+open FunScript
+open FunScript.Bindings
+open FunScript.Bindings.D3
+
+module D3Tests = 
+    let u2_1<'t> = U2<'t,_>.Case1
+    [<JS>]
+    let scale() =  d3.scaleLinear().domain([|U2.Case1 10.0; U2.Case1 130.0|]).range([|0.0; 960.0|]);
+
+    [<Fact>]
+    let ``Can create D3 axis`` () =    
+        let s = compile <@ scale @>
+        Assert.NotNull s
+                   
