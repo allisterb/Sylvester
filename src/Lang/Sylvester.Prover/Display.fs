9d36c31fe7b57b91b21f6689b5685a2904e385a8
Allister Beharry <allister.beharry@gmail.com>
2020-05-23 19:26:17 -0400

    Remove redundant functions.

--- src/Lang/Sylvester.Prover/Display.fs
+++ src/Lang/Sylvester.Prover/Display.fs
@@ -11,20 +11,14 @@ module FormulaDisplay =
         | Some i -> let a = info.Value.GetCustomAttributes(typeof<UnicodeAttribute>, true) in
                     if a = null || a.Length = 0 then None else let u = (a.[0] :?> UnicodeAttribute) in u.Symbol |> Some
         | _ -> None
-    
-    let replace_with_op_unicode_display op =
-        let s = src op
-        match get_op_unicode_display op with
-        | Some t ->  t
-        | None -> s
+
+    let get_op_unicode_display' (info:MethodInfo) =
+        let a = info.GetCustomAttributes(typeof<UnicodeAttribute>, true) in
+        if a = null || a.Length = 0 then None else let u = (a.[0] :?> UnicodeAttribute) in u.Symbol |> Some
+        
+    let (|UnicodeDisplay|_|) (info:MethodInfo) = get_op_unicode_display' info 
 
     let rec print_formula expr = 
         match expr with
-        | BinaryFormula(info, l, r) -> 
-            let a = info.GetCustomAttributes(typeof<UnicodeAttribute>, true) in
-            if a = null || a.Length = 0 then 
-                src expr
-            else 
-                let symbol = let u = (a.[0] :?> UnicodeAttribute) in u.Symbol
-                sprintf "%s %s %s" (print_formula l) (symbol) (print_formula r) 
+        | BinaryFormula(UnicodeDisplay(symbol), l, r) -> sprintf "%s %s %s" (print_formula l) (symbol) (print_formula r) 
         | expr -> src expr
