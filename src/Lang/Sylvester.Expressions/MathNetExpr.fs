1a1b891e00972967b2b503405338f09f872e5993
Allister Beharry <allister.beharry@gmail.com>
2020-07-29 16:01:42 -0400

    Add remaining polynomial ops to MathNt Expression->Quotation method.

--- src/Lang/Sylvester.Expressions/MathNetExpr.fs
+++ src/Lang/Sylvester.Expressions/MathNetExpr.fs
@@ -1,6 +1,7 @@
 ﻿namespace Sylvester
 
 open System
+open System.Reflection
 
 open Microsoft.FSharp.Quotations
 open Microsoft.FSharp.Quotations.Patterns
@@ -8,6 +9,9 @@ open Microsoft.FSharp.Quotations.DerivedPatterns
 
 open MathNet.Numerics
 open MathNet.Symbolics
+open MathNet.Symbolics.ExpressionPatterns
+open MathNet.Symbolics.Operators
+
 
 module MathNetExpr =
     
@@ -34,7 +38,16 @@ module MathNetExpr =
         | Lambda (_, t) -> fromQuotation t
         | _ -> failwith "not supported"
 
-    let toQuotation (expr: Expression) (vars: Var list) (valueType: Type) =
+    let rec toQuotation<'t> (expr: Expression) (vars: Var list) =    
+        let rec numerator = function
+            | NegPower _ -> one
+            | Product ax -> product <| List.map numerator ax
+            | z -> z
+        let rec denominator = function
+            | NegPower (r, p) -> r ** -p
+            | Product ax -> product <| List.map denominator ax
+            | _ -> one
+
         let value = function
             | Value.Approximation a -> Expr.Value a.RealValue |> Some
             | Value.NegativeInfinity -> Expr.Value Double.NegativeInfinity |> Some
@@ -45,7 +58,6 @@ module MathNetExpr =
             | E -> Expr.Value Constants.E  |> Some 
             | Pi -> Expr.Value Constants.Pi |> Some
             | _ -> None
-        let mathType = typeof<System.Math>
         let argName = function |Symbol(n) -> n
         let getParam p =
             List.fold(
@@ -55,19 +67,148 @@ module MathNetExpr =
                         | Some(v) -> Some v
                         | _ -> None
                     ) None vars
-        
-        let add a b = <@@ (%%a:float) + (%%b:float) @@>
-                
+        let rec getMethodInfo = function
+            | Call(None, methodInfo, _) -> methodInfo
+            | Lambda(_, expr) -> getMethodInfo expr
+            | _ -> failwith "Expression is not a function."
+
+        let add (a:Expr) (b:Expr) = 
+            do if a.Type <> b.Type then failwithf "The type of the LHS:%A is not the type of the RHS: %A " a.Type b.Type
+            match a.Type.Name with
+            | "Int32" -> <@@ (%%a:int) + (%%b:int) @@>
+            | "Int64" -> <@@ (%%a:int64) + (%%b:int64) @@>
+            | "Double" -> <@@ (%%a:float) + (%%b:float) @@>
+            | "Single" -> <@@ (%%a:float32) + (%%b:float32) @@>
+            | "BigRational" -> <@@ (%%a:BigRational) + (%%b:BigRational) @@>
+            | "BigInteger" -> <@@ (%%a:BigInteger) + (%%b:BigInteger) @@>
+            | n -> failwithf "Addition operation for type %A not supported." a.Type 
+    
+        let mul (a:Expr) (b:Expr) = 
+            do if a.Type <> b.Type then failwithf "The type of the LHS:%A is not the type of the RHS: %A " a.Type b.Type
+            match a.Type.Name with
+            | "Int32" -> <@@ (%%a:int) * (%%b:int) @@>
+            | "Int64" -> <@@ (%%a:int64) * (%%b:int64) @@>
+            | "Double" -> <@@ (%%a:float) * (%%b:float) @@>
+            | "Single" -> <@@ (%%a:float32) * (%%b:float32) @@>
+            | "BigRational" -> <@@ (%%a:BigRational) * (%%b:BigRational) @@>
+            | "BigInteger" -> <@@ (%%a:BigInteger) * (%%b:BigInteger) @@>
+            | n -> failwithf "Multiplication operation for type %A not supported." a.Type
+    
+        let sub (a:Expr) (b:Expr) = 
+            do if a.Type <> b.Type then failwithf "The type of the LHS:%A is not the type of the RHS: %A " a.Type b.Type
+            match a.Type.Name with
+            | "Int32" -> <@@ (%%a:int) - (%%b:int) @@>
+            | "Int64" -> <@@ (%%a:int64) - (%%b:int64) @@>
+            | "Double" -> <@@ (%%a:float) - (%%b:float) @@>
+            | "Single" -> <@@ (%%a:float32) - (%%b:float32) @@>
+            | "BigRational" -> <@@ (%%a:BigRational) - (%%b:BigRational) @@>
+            | "BigInteger" -> <@@ (%%a:BigInteger) - (%%b:BigInteger) @@>
+            | n -> failwithf "Subtraction operation for type %A not supported." a.Type
+    
+        let div (a:Expr) (b:Expr) = 
+            do if a.Type <> b.Type then failwithf "The type of the LHS:%A is not the type of the RHS: %A " a.Type b.Type
+            match a.Type.Name with
+            | "Int32" -> <@@ (%%a:int) / (%%b:int) @@>
+            | "Int64" -> <@@ (%%a:int64) / (%%b:int64) @@>
+            | "Double" -> <@@ (%%a:float) / (%%b:float) @@>
+            | "Single" -> <@@ (%%a:float32) / (%%b:float32) @@>
+            | "BigRational" -> <@@ (%%a:BigRational) / (%%b:BigRational) @@>
+            | "BigInteger" -> <@@ (%%a:BigInteger) / (%%b:BigInteger) @@>
+            | n -> failwithf "Addition operation for type %A not supported." a.Type
+
         let rec convertExpr : Expression -> Expr option = 
             function
-            | Identifier(sym) -> Option.map (fun x -> Expr.Var(x)) (getParam sym)
+            | Identifier(sym) -> (getParam sym) |> Option.map (fun x -> Expr.Var(x))
             | Values.Value v -> value v
             | Constant c -> constant c
             | Sum(xs) ->
                 let summands = List.map convertExpr xs
                 List.fold (Option.map2 add) (value Value.zero) summands
+            | Product(_) as p ->
+                let n = numerator p
+                let d = denominator p
+                if isOne d then
+                    compileFraction n
+                else
+                    let nExp = compileFraction n
+                    let dExp = compileFraction d
+                    Option.map2 div nExp dExp
+            | Function (func, par) ->
+                let convertFunc : Function -> MethodInfo option = function
+                    | Sin  -> getMethodInfo <@ Math.Sin @> |> Some
+                    | Cos  -> getMethodInfo <@ Math.Cos @> |> Some
+                    | Tan  -> getMethodInfo <@ Math.Tan @> |> Some
+                    //| Csc  -> getMethodInfo <@ Math.Csc @> |> Some
+                    //| Sec  -> getMethodInfo <@ Math.Sec @> |> Some
+                    //| Cot -> getMethodInfo <@ Math.Cot @> |> Some
+                    | Sinh -> getMethodInfo <@ Math.Sinh @> |> Some
+                    | Cosh -> getMethodInfo <@ Math.Cosh @> |> Some
+                    | Tanh -> getMethodInfo <@ Math.Tanh @> |> Some
+                    //| Csch -> getMethodInfo <@ Math.Csch @> |> Some
+                    //| Sech  -> Some (mathCall1 "Sech")
+                    //| Coth  -> Some (mathCall1 "Coth")
+                    | Asin -> getMethodInfo <@ Math.Asin @> |> Some
+                    | Acos -> getMethodInfo <@ Math.Acos @> |> Some                
+                    | Atan -> getMethodInfo <@ Math.Atan @> |> Some
+                    //| Acsc -> Some (mathCall1 "Acsc")
+                    //| Asec -> Some (mathCall1 "Asec")
+                    //| Acot -> Some (mathCall1 "Acot")
+                    //| Asinh -> Some (mathCall1 "Asinh")
+                    //|Acosh -> Some (mathCall1 "Acosh")
+                    //| Atanh -> Some (mathCall1 "Atanh")
+                    //| Acsch -> Some (mathCall1 "Acsch")
+                    //| Asech -> Some (mathCall1 "Asech")
+                    //| Acoth -> Some (mathCall1 "Acoth")
+                    //| AiryAi -> Some (mathCall1 "AiryAi")
+                    //| AiryAiPrime -> Some (mathCall1 "AiryAiPrime")
+                    //| AiryBi -> Some (mathCall1 "AiryBi")
+                    //| AiryBiPrime -> Some (mathCall1 "AiryBiPrime")
+                    | Ln   -> getMethodInfo <@ Math.Log @> |> Some
+                    | Log   -> getMethodInfo <@ Math.Log10 @> |> Some
+                    | Exp  -> getMethodInfo <@ Math.Exp @> |> Some
+                    //| Abs  -> getMethodInfo <@ Math.Abs @> |> Some
+                    | _    -> None
+                let f = convertFunc func
+                let e = convertExpr par
+                Option.map2 (fun x y -> Expr.Call(x, [y])) f e
+            | PosIntPower(x, Number(y)) ->
+                let basis = convertExpr x
+                let rec exponentiate (power : BigRational) exp  =
+                    if  power.Numerator.IsEven then
+                        let newBasis = mul exp exp
+                        exponentiate (power / (BigRational.FromInt(2))) newBasis
+                    elif power = 1N then
+                        exp
+                    else
+                        let newBasis = exponentiate (power - BigRational.One) exp
+                        mul exp newBasis
+                Option.map (exponentiate y) basis
+            | Power(x, minusOne) when minusOne = Expression.MinusOne ->
+                let a = convertExpr x
+                Option.map2 div (value Value.one) a
+            | Power (x, Power(n, minusOne)) when minusOne = Expression.MinusOne ->
+                let a = convertExpr x
+                let b = convertExpr (Power(n, minusOne))
+                if n = Operators.two then
+                    Option.map (fun x -> Expr.Call(getMethodInfo <@ Math.Sqrt @>, [x])) a
+                else
+                    let a = convertExpr x
+                    let b = convertExpr (Power(n, minusOne))
+                    Option.map2 (fun x y -> Expr.Call (getMethodInfo <@ Math.Pow @>, x::y::[])) a b
+            | Power(Constant E, y) ->
+                let exponent = convertExpr y
+                Option.map (fun x -> Expr.Call(getMethodInfo <@ Math.Exp @>, [x])) exponent
+            | Power(x, y) ->
+                let baseE = convertExpr x
+                let exponE = convertExpr y
+                Option.map2 (fun x y -> Expr.Call(getMethodInfo <@ Math.Sqrt @>, x::y::[])) baseE exponE
             | _ -> None
+        and compileFraction = 
+            function
+            | Product(xs) ->
+                let prods = List.map convertExpr xs
+                List.fold (Option.map2 mul) (value Value.one) prods
+            | x -> convertExpr x
 
-        let a = 4N / 5N;
         convertExpr expr
         
--- tests/Sylvester.Tests.CAS.MathNet/Tests.fs
+++ tests/Sylvester.Tests.CAS.MathNet/Tests.fs
@@ -1,26 +1,20 @@
 namespace Sylvester.Tests.CAS
 
-open Sylvester
-open MathNet.Symbolics
-
 open FSharp.Quotations
-open FSharp.Quotations.Patterns
-open FSharp.Quotations.DerivedPatterns
 
+open Xunit
+open MathNet.Symbolics
+open MathNet.Symbolics.Operators
+
+open Sylvester
 
 module MathNet = 
-    open System
-    open Xunit
-
     let var<'t> = Unchecked.defaultof<'t>
 
     [<Fact>]
     let ``My test`` () =
-        let j = Infix.parseOrThrow "x + 1"
-        
-        let x = var<float>
-        let q = <@ x @>
+        let x = symbol "x"
         let v = [Var("x", typeof<float>)]
        
-        let res = MathExpr.toQuotation j v typeof<float>
+        let res = MathNetExpr.toQuotation (x + 2) v 
         Assert.True(true)
