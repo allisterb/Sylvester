2b905669d09ffe235347f9df6c571f7427a5859a
Allister Beharry <allister.beharry@gmail.com>
2021-04-27 11:40:21 -0400

    Use Z3Solver type.

--- src/Lang/Solvers/Sylvester.Solver.Z3/Z3.fs
+++ src/Lang/Solvers/Sylvester.Solver.Z3/Z3.fs
@@ -9,11 +9,20 @@ open FSharp.Quotations.DerivedPatterns
 
 open Microsoft.Z3
 
+type Z3ModelResult =
+| ConstResult of Expr
+| FuncResult of FuncInterp
+
+type Z3Solver(?logic:string) =
+    let ctx = new Context()
+    let solver = logic |> function | Some l -> ctx.MkSolver(l )| None -> ctx.MkSolver()    
+    member val Ctx = ctx
+    member val Solver = solver
+    member val Tactics = ctx.TacticNames
+    member x.Check(constraints: seq<BoolExpr>) = solver.Check constraints
+    member x.Model() = solver.Model
+
 module Z3 =
-    type ModelResult =
-    | ConstResult of Expr
-    | FuncResult of FuncInterp
-    
     /// Multiple indexers for evaluating formulas
     type Microsoft.Z3.Model with
       member x.Item (index: Expr) =
@@ -25,53 +34,55 @@ module Z3 =
         else 
             x.FuncInterp(index) |> FuncResult
     
-    let create_numeral (ctx:Context) (i:obj)  = 
+    let create_numeral (solver:Z3Solver) (i:obj)  = 
         match i with
-        | :? uint64  as n -> n |> ctx.MkInt :> ArithExpr
-        | :? int64 as n -> n |> ctx.MkInt :> ArithExpr
-        | :? uint32 as n -> n |> ctx.MkInt :> ArithExpr
-        | :? int32 as n -> n |> ctx.MkInt  :> ArithExpr
-        | :? Rational as n -> ctx.MkReal((int) n.Numerator, (int) n.Denominator) :> ArithExpr
-        | :? real as n -> ctx.MkReal(n.ToString()) :> ArithExpr
+        | :? uint64  as n -> n |> solver.Ctx.MkInt :> ArithExpr
+        | :? int64 as n -> n |> solver.Ctx.MkInt :> ArithExpr
+        | :? uint32 as n -> n |> solver.Ctx.MkInt :> ArithExpr
+        | :? int32 as n -> n |> solver.Ctx.MkInt  :> ArithExpr
+        | :? Rational as n -> solver.Ctx.MkReal((int) n.Numerator, (int) n.Denominator) :> ArithExpr
+        | :? real as n -> solver.Ctx.MkReal(n.ToString()) :> ArithExpr
         | _ -> failwithf "Cannot create numeral from value %A of type %A." i (i.GetType())
 
-    let create_const (ctx:Context) (s:string) (t:Type) = 
+    let create_const (solver:Z3Solver) (s:string) (t:Type) = 
         match t.Name with
         | "UInt32 "
         | "Int32"
         | "UInt64"
-        | "Int64" -> (s, s |> ctx.MkIntConst :> ArithExpr)
+        | "Int64" -> (s, s |> solver.Ctx.MkIntConst :> ArithExpr)
         | "Single"
         | "Double"
-        | "Rational" -> (s, s |> ctx.MkRealConst :> ArithExpr) 
+        | "Rational" -> (s, s |> solver.Ctx.MkRealConst :> ArithExpr) 
         | t -> failwithf "Cannot create constant of type %A." t
 
-    let rec create_arith_expr (ctx:Context) (expr:FSharp.Quotations.Expr) : ArithExpr =
-        let vars = expr |> expand |> get_vars |> List.map(fun v -> create_const ctx v.Name v.Type) |> Map.ofList 
+    let rec create_arith_expr (solver:Z3Solver) (expr:FSharp.Quotations.Expr) : ArithExpr =
+        let vars = expr |> expand |> get_vars |> List.map(fun v -> create_const solver v.Name v.Type) |> Map.ofList 
         match expr with
         | Var v -> vars.[v.Name] 
-        | ValueWithName(v,_,_) -> create_numeral ctx v
-        | UInt32 n -> create_numeral ctx n
-        | Int32 n -> create_numeral ctx n
-        | UInt64 n -> create_numeral ctx n
-        | Int64 n -> create_numeral ctx n
-        | Single n -> create_numeral ctx n
-        | Double n -> create_numeral ctx n
-        | Rational n -> create_numeral ctx n
-        | Call(None, Op "op_Addition" ,l::r::[]) -> ctx.MkAdd((create_arith_expr ctx l), (create_arith_expr ctx r))
-        | Call(None, Op "op_Multiply" ,l::r::[]) -> ctx.MkMul((create_arith_expr ctx l), (create_arith_expr ctx r))
-        | Call(None, Op "op_Subtraction" ,l::r::[]) -> ctx.MkSub((create_arith_expr ctx l), (create_arith_expr ctx r))
-        | Call(None, Op "op_Division" ,l::r::[]) -> ctx.MkDiv((create_arith_expr ctx l), (create_arith_expr ctx r))
+        | ValueWithName(v,_,_) -> create_numeral solver v
+        | UInt32 n -> create_numeral solver n
+        | Int32 n -> create_numeral solver n
+        | UInt64 n -> create_numeral solver n
+        | Int64 n -> create_numeral solver n
+        | Single n -> create_numeral solver n
+        | Double n -> create_numeral solver n
+        | Rational n -> create_numeral solver n
+        | Call(None, Op "FromOne" , []) as e when e.Type = typeof<Rational> -> create_numeral solver 1Q
+        | Call(None, Op "FromZero" , []) as e when e.Type = typeof<Rational> -> create_numeral solver 0Q
+        | Call(None, Op "op_Addition" ,l::r::[]) -> solver.Ctx.MkAdd((create_arith_expr solver l), (create_arith_expr solver r))
+        | Call(None, Op "op_Multiply" ,l::r::[]) -> solver.Ctx.MkMul((create_arith_expr solver l), (create_arith_expr solver r))
+        | Call(None, Op "op_Subtraction" ,l::r::[]) -> solver.Ctx.MkSub((create_arith_expr solver l), (create_arith_expr solver r))
+        | Call(None, Op "op_Division" ,l::r::[]) -> solver.Ctx.MkDiv((create_arith_expr solver l), (create_arith_expr solver r))
         | e when e.Type = typeof<bool>-> failwithf "The expression %A is a boolean expression." e
         | e  -> failwithf "The expression %A of type %A is not an arithmetic expression." e (e.Type)
 
-    let rec create_bool_expr (ctx:Context) (expr:FSharp.Quotations.Expr) : BoolExpr =
+    let rec create_bool_expr (solver:Z3Solver) (expr:FSharp.Quotations.Expr) : BoolExpr =
         let vars = 
             expr
             |> expand
             |> get_vars 
             |> List.choose(fun v -> if v.Type = typeof<bool> then Some v else None)
-            |> List.map(fun v -> v.Name, ctx.MkBoolConst(v.Name)) |> Map.ofList
+            |> List.map(fun v -> v.Name, solver.Ctx.MkBoolConst(v.Name)) |> Map.ofList
         match expr.Type.Name with
             | "Boolean"
             | "UInt32 "
@@ -83,48 +94,41 @@ module Z3 =
             | "Rational" ->
                 match expr with
                 | Var v when v.Type = typeof<bool> -> vars.[v.Name]
-                | ValueWithName(v, t, _) when t = typeof<bool> -> ctx.MkBool(v :?> bool)
-                | Bool true -> ctx.MkTrue()
-                | Bool false -> ctx.MkFalse()
-                | Call(None, Op "op_Equality" ,l::r::[]) when l.Type = typeof<bool> -> ctx.MkEq(create_bool_expr ctx l, create_bool_expr ctx r)
-                | Call(None, Op "op_Inequality" ,l::r::[]) when l.Type = typeof<bool> -> ctx.MkDistinct(create_bool_expr ctx l, create_bool_expr ctx r)
-                | Call(None, Op "op_BarAmpBar" ,l::r::[]) -> ctx.MkAnd(create_bool_expr ctx l, create_bool_expr ctx r)
-                | Call(None, Op "op_BitwiseOr" ,l::r::[]) -> ctx.MkOr(create_bool_expr ctx l, create_bool_expr ctx r)
-                | Call(None, Op "Not" ,r::[]) -> ctx.MkNot(create_bool_expr ctx r)
-                | Call(None, Op "op_EqualsEqualsGreater" ,l::r::[]) -> ctx.MkImplies(create_bool_expr ctx l, create_bool_expr ctx r)
+                | ValueWithName(v, t, _) when t = typeof<bool> -> solver.Ctx.MkBool(v :?> bool)
+                | Bool true -> solver.Ctx.MkTrue()
+                | Bool false -> solver.Ctx.MkFalse()
+                | Call(None, Op "op_Equality" ,l::r::[]) when l.Type = typeof<bool> -> solver.Ctx.MkEq(create_bool_expr solver l, create_bool_expr solver r)
+                | Call(None, Op "op_Inequality" ,l::r::[]) when l.Type = typeof<bool> -> solver.Ctx.MkDistinct(create_bool_expr solver l, create_bool_expr solver r)
+                | Call(None, Op "op_BarAmpBar" ,l::r::[]) -> solver.Ctx.MkAnd(create_bool_expr solver l, create_bool_expr solver r)
+                | Call(None, Op "op_BitwiseOr" ,l::r::[]) -> solver.Ctx.MkOr(create_bool_expr solver l, create_bool_expr solver r)
+                | Call(None, Op "Not" ,r::[]) -> solver.Ctx.MkNot(create_bool_expr solver r)
+                | Call(None, Op "op_EqualsEqualsGreater" ,l::r::[]) -> solver.Ctx.MkImplies(create_bool_expr solver l, create_bool_expr solver r)
                 
-                | Call(None, Op "op_Equality" ,l::r::[]) -> ctx.MkEq(create_arith_expr ctx l, create_arith_expr ctx r)
-                | Call(None, Op "op_Inequality" ,l::r::[]) -> ctx.MkDistinct(create_arith_expr ctx l, create_arith_expr ctx r)
-                | Call(None, Op "op_LessThan" ,l::r::[]) -> ctx.MkLt(create_arith_expr ctx l, create_arith_expr ctx r)
-                | Call(None, Op "op_LessThanOrEqual" ,l::r::[]) -> ctx.MkLe(create_arith_expr ctx l, create_arith_expr ctx r)
-                | Call(None, Op "op_GreaterThan" ,l::r::[]) -> ctx.MkGt(create_arith_expr ctx l, create_arith_expr ctx r)
-                | Call(None, Op "op_GreaterThanOrEqual" ,l::r::[]) -> ctx.MkGe(create_arith_expr ctx l, create_arith_expr ctx r)
+                | Call(None, Op "op_Equality" ,l::r::[]) -> solver.Ctx.MkEq(create_arith_expr solver l, create_arith_expr solver r)
+                | Call(None, Op "op_Inequality" ,l::r::[]) -> solver.Ctx.MkDistinct(create_arith_expr solver l, create_arith_expr solver r)
+                | Call(None, Op "op_LessThan" ,l::r::[]) -> solver.Ctx.MkLt(create_arith_expr solver l, create_arith_expr solver r)
+                | Call(None, Op "op_LessThanOrEqual" ,l::r::[]) -> solver.Ctx.MkLe(create_arith_expr solver l, create_arith_expr solver r)
+                | Call(None, Op "op_GreaterThan" ,l::r::[]) -> solver.Ctx.MkGt(create_arith_expr solver l, create_arith_expr solver r)
+                | Call(None, Op "op_GreaterThanOrEqual" ,l::r::[]) -> solver.Ctx.MkGe(create_arith_expr solver l, create_arith_expr solver r)
                 
                 | _ -> failwithf "Cannot create Z3 expression fron expression %A of type %A." expr (expr.Type)
             
             | _ -> failwithf "Cannot create Z3 expression fron expression %A of type %A." expr (expr.Type)
 
-    let create_ctx() = new Context()
 
-    let tactics = let ctx = create_ctx() in ctx.TacticNames
-
-    let create_solver (ctx:Context) = ctx.MkSolver()
-
-    let create_solver' (ctx:Context) (logic:string) = ctx.MkSolver(logic)
-
-    let check_sat (ctx:Context) (s:Solver) (a: Expr<bool list>) = 
-        let sol = a |> expand_list |> List.map (create_bool_expr ctx) |> s.Check
+    let check_sat (s:Z3Solver) (a: Expr<bool list>) = 
+        let sol = a |> expand_list |> List.map (create_bool_expr s) |> s.Check
         match sol with
         | Status.SATISFIABLE -> true
         | _ -> false
 
-    let check_sat_model (ctx:Context) (s:Solver) (a: Expr<bool list>) = 
-        let sol = a |> expand_list |> List.map (create_bool_expr ctx) |> s.Check 
+    let check_sat_model (s:Z3Solver) (a: Expr<bool list>) = 
+        let sol = a |> expand_list |> List.map (create_bool_expr s) |> s.Check 
         match sol with
-        | Status.SATISFIABLE -> Some s.Model
+        | Status.SATISFIABLE -> Some (s.Model())
         | _ -> None
 
-    let get_model_consts (m:Model) = 
+    let get_var_model (m:Model) = 
         m.ConstDecls 
         |> Array.toList 
         |> List.map(fun c -> c.Name, match  m.[c] with | ConstResult c -> c  | _ -> failwith "This not a constant result.")
@@ -139,4 +143,14 @@ module Z3 =
         | e when e.IsRatNum -> let r = (e :?> RatNum) in Rational(r.BigIntNumerator, r.BigIntDenominator)
         | e -> failwithf "The expression %A is not a rational constant." e 
         
-    let get_int_model_consts : Model-> int list = get_model_consts >> List.map(snd >> get_int_const)
+    let get_int_var_model : Model-> (string *int) list = 
+        get_var_model >> List.map(fun (l, r) -> l.ToString(), get_int_const r)
+
+    let get_rat_var_model : Model-> (string * Rational) list = 
+        get_var_model >> List.map(fun (l, r) -> l.ToString(), get_rat_const r)
+
+    //let create_sort (ctx:Context) (t:Type) =
+    //    match t.Name with
+    //    | "UInt32" -> ctx.MkIntSort() :> Sort
+    //    | "Rational" -> ctx.MkRealSort() :> Sort
+ 
--- tests/Sylvester.Tests.Solver.Z3/Z3Tests.fs
+++ tests/Sylvester.Tests.Solver.Z3/Z3Tests.fs
@@ -7,55 +7,53 @@ open Xunit
 open Sylvester 
 open Z3
 
-
 module Z3Tests =      
+
     [<Fact>]
     let ``Can create arithmetic expr``() =
         let x = var'<int> "x"
         let x' = var'<real> "xx"
         let y' = var'<real> "y"
-        let ctx = create_ctx()
+        let s = Z3Solver()
         
-        Assert.NotNull <| create_arith_expr ctx <@ %x + 2 @>
-        Assert.NotNull <| create_arith_expr ctx <@ %x' + %y' * 2. @>
+        Assert.NotNull <| create_arith_expr s <@ %x + 2 @>
+        Assert.NotNull <| create_arith_expr s <@ %x' + %y' * 2. @>
 
     [<Fact>]
     let ``Can create bool expr``() =
         let x = var'<int> "x"
         let b = var'<bool> "b"
-        let ctx = create_ctx()
-        
-        Assert.NotNull <| create_bool_expr ctx <@ %x > %x + 2 @>
-        Assert.NotNull <| create_bool_expr ctx <@ %b ==> false @>
+        let s = Z3Solver()
+        Assert.NotNull <| create_bool_expr s <@ %x > %x + 2 @>
+        Assert.NotNull <| create_bool_expr s <@ %b ==> false @>
 
     [<Fact>]
     let ``Can check sat``() =
         let x = var'<real> "x"
         let y = var'<real> "y"
         let b = var'<bool> "b"
-        let ctx = create_ctx()
-        let solver = create_solver ctx
+       
+        let solver = Z3Solver()
         let a = <@ [%x = 6.; %x > 5.] @>  
-        Assert.True <| check_sat ctx solver a
+        Assert.True <| check_sat solver a
 
     [<Fact>]
     let ``Can solve``() =
          let x = var'<int> "x"
          let y = var'<int> "y"
          let b = var'<bool> "b"
-         let ctx = create_ctx()
-         let solver = create_solver ctx
+         
+         let solver = Z3Solver()
          let a = <@ [%x = 5; %y = %x + 2] @>  
-         let sol = check_sat_model ctx solver a
+         let sol = check_sat_model solver a
          Assert.True sol.IsSome
-         let rr = get_int_model_consts sol.Value
+         let rr = get_int_var_model sol.Value
          Assert.NotEmpty rr
-        
-    [<Fact>]
-    let ``Can get tactics``() =
-         let t =  tactics
-         Assert.NotEmpty t
 
+         let qx = var'<Rational> "qx"
+         let qy = var'<Rational> "qy"
+         let sol2 = check_sat_model solver <@[%qx = 1Q ]@>
+         Assert.True sol2.IsSome
 
 
  
