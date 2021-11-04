a5fe60ec60398a3b5ff0aa8d28d8eff468d38f80
Allister Beharry <allister.beharry@gmail.com>
2021-11-04 12:41:22 -0400

    Update PropCalculus proofs.

--- docs/examples/proofs/PropCalculus.fsx
+++ docs/examples/proofs/PropCalculus.fsx
@@ -5,7 +5,7 @@ open PropCalculus
 
 let p,q,r,s = boolvar4 "p" "q" "r" "s"
 let P,N,A,S = boolvar4 "P" "N" "A" "S"
- 
+
 let ``3.52`` = theorem prop_calculus <@ (%p = %q) = (%p |&| %q) ||| (not %p |&| not %q) @> [
         collect |> apply_right
         commute |> apply_left |> after_left
@@ -82,23 +82,23 @@ let ``3.76c`` = theorem prop_calculus <@ %p |&| %q ==> %p ||| %q @> [
 theorem prop_calculus <@  (%p |&| %q ) ==> %p @> [
     def_implies |> apply
     commute |> apply_left
-    absorb_or p q |> Lemma' |> apply
+    absorb_or p q |> Lemma'
 ]
 
 theorem prop_calculus <@ %p ==> true @> [
     def_implies |> apply
     zero_or p |> apply_left
-    //ident_eq <@ true @> |> L
+    ident_eq <@ true @> |> apply_left
 ]
 
-let ``3.76d`` = theorem prop_calculus <@ (p ||| (q |&| r)) ==> (p ||| q) @> [
-    distrib |> L
-    strengthen_and <@ p ||| q @> <@ p ||| r @> |> Lemma
+let ``3.76d`` = theorem prop_calculus <@ (%p ||| (%q |&| %r)) ==> (%p ||| %q) @> [
+    distrib |> apply_left
+    strengthen_and <@ %p ||| %q @> <@ %p ||| %r @> |> Lemma
 ]
 
-let ``3.76e`` = theorem prop_calculus <@ (p |&| q)  ==> (p |&| (q ||| r)) @> [
-    distrib |> R
-    weaken_or <@ p |&| q @> <@ p |&| r @> |> Lemma
+let ``3.76e`` = theorem prop_calculus <@ (%p |&| %q)  ==> (%p |&| (%q ||| %r)) @> [
+    distrib |> apply_right
+    weaken_or <@ %p |&| %q @> <@ %p |&| %r @> |> Lemma
 ]
 
 (*
@@ -108,123 +108,121 @@ let ``3.77`` = theorem prop_calculus <@ p |&| (p ==> q) ==> q @> [
     strengthen_and q' p' |> Taut |> LR
 ]
 *)
-let ``3.78`` = theorem prop_calculus <@( p ==> r) |&| (q ==> r) = (p ||| q  ==> r) @> [
-    ident_implies_not_or <@p ||| q@> r' |> R
-    distrib|> L |> R'
-    distrib_or_and r' <@ not p @> <@ not q@> |> CommuteL |> R
-    commute |> L |> R'
-    commute |> R |> R'
-    ident_implies_not_or p' r' |> Commute |> R
-    ident_implies_not_or q' r' |> Commute |> R
+let ``3.78`` = theorem prop_calculus <@ (%p ==> %r) |&| (%q ==> %r) = (%p ||| %q  ==> %r) @> [
+    ident_implies_not_or <@ %p ||| %q @> r |> apply_right
+    distrib|> apply_left |> after_right
+    distrib_or_and r <@ not %p @> <@ not %q@> |> CommuteL |> apply_right
+    commute |> apply_left |> after_right
+    commute |> apply_right |> after_right
+    ident_implies_not_or p r |> Commute |> apply_right
+    ident_implies_not_or q r |> Commute |> apply_right
 ]
 
-let ``3.79`` = theorem prop_calculus <@ (p ==> r) |&| (not p ==> r ) = r @> [
-    case_analysis_1 p' <@not p @> r' |> LR
-    excluded_middle |> L |> L'
-    ident_conseq_true r' |> Lemma'
+let ``3.79`` = theorem prop_calculus <@ (%p ==> %r) |&| (not %p ==> %r ) = %r @> [
+    case_analysis_1 p <@ not %p @> r |> apply
+    excluded_middle |> apply_left |> after_left
+    ident_conseq_true r |> Lemma'
 ]
 
-let ``3.81`` = theorem prop_calculus <@ (p ==> q) |&| (q ==> p) ==> (p = q) @> [
-  mutual_implication' p' q' |> L  
-  reflex_implies <@ p = q @> |> Lemma
+let ``3.81`` = theorem prop_calculus <@ (%p ==> %q) |&| (%q ==> %p) ==> (%p = %q) @> [
+  mutual_implication' p q |> apply_left  
+  reflex_implies <@ %p = %q @> |> Lemma
 ] 
 
-let ``3.82b`` = theorem prop_calculus <@ (p = q) |&| (q ==> r) ==> (p ==> r) @> [
-    mutual_implication' p' q' |> Commute |> L
-    rshunt |> LR
-    commute |> L
-    left_assoc |> L
-    left_assoc |> L |> L'
-    ident_and_implies p' q' |> L |> L'
-    right_assoc |> L |> L'
-    ident_and_implies q' p' |> L |> L'
-    commute |> R |> L' |> L'
-    left_assoc |> L |> L'
-    idemp_and p' |> L |> L'
-    right_assoc |> L
-    ident_and_implies q' r' |> L
-    left_assoc |> L
-    commute |> L
-    strengthen_and r' <@ p |&| q @> |> Lemma
-]
-let ``3.84`` = theorem prop_calculus <@ p |&| q ==> r = (p ==> (q ==> r)) @> [
-    ident_implies_eq_and_eq <@ p |&| q@> r' |> L
-    ident_implies_eq_and_eq q' r' |> R
-    ident_implies_eq_and_eq p' <@q |&| r = q@> |> R
-    distrib_and_eq p' <@q |&| r@> <@ q @> |> R
-    left_assoc_and p' q' r' |> R
-    right_assoc |> R
-    def_true p' |> Commute |> R
-    left_assoc |> LR
-    commute |> LR
+let ``3.82b`` = theorem prop_calculus <@ (%p = %q) |&| (%q ==> %r) ==> (%p ==> %r) @> [
+    mutual_implication' p q |> Commute |> apply_left
+    rshunt |> apply
+    commute |> apply_left
+    left_assoc |> apply_left
+    left_assoc |> apply_left |> after_left
+    ident_and_implies p q |> apply_left |> after_left
+    right_assoc |> apply_left |> after_left
+    ident_and_implies q p |> apply_left |> after_left
+    commute |> apply_right |> after_left |> after_left
+    left_assoc |> apply_left |> after_left
+    idemp_and p |> apply_left |> after_left
+    right_assoc |> apply_left
+    ident_and_implies q r |> apply_left
+    left_assoc |> apply_left
+    commute |> apply_left
+    strengthen_and r <@ %p |&| %q @> |> Lemma
 ]
 
-let ``3.84a`` = theorem prop_calculus <@ p |&| q ==> (p |&| (q ||| r)) @> [
-    subst_true |> LR
-    commute |> L
-    subst_true |> LR
-    distrib |> R
-    idemp_and <@ true @> |> L |> R'
-    zero_or <@ true |&| r@> |> CommuteL |> R
-    implies_true <@ q |&| p @> |> Lemma
+let ``3.84`` = theorem prop_calculus <@ %p |&| %q ==> %r = (%p ==> (%q ==> %r)) @> [
+    ident_implies_eq_and_eq <@ %p |&| %q @> r |> apply_left
+    ident_implies_eq_and_eq q r |> apply_right
+    ident_implies_eq_and_eq p <@ %q |&| %r = %q @> |> apply_right
+    distrib_and_eq p <@ %q |&| %r @> q |> apply_right
+    left_assoc_and p q r |> apply_right
+    right_assoc |> apply_right
+    def_true p |> Commute |> apply_right
+    left_assoc |> apply
+    commute |> apply
 ]
 
-let ``3.84b`` = theorem prop_calculus <@ p ==> (q ==> p) @> [
-    def_implies |> R
-    def_implies |> LR
-    commute |> L |> R' |> L' 
-    distrib |> L
-    left_assoc |> L |> L'
-    idemp_or p' |> L |> L' |> L'
-    commute |> L |> L' 
-    idemp_or p' |> R |> L'
+let ``3.84a`` = theorem prop_calculus <@ %p |&| %q ==> (%p |&| (%q ||| %r)) @> [
+    subst_true |> apply
+    commute |> apply_left
+    subst_true |> apply
+    distrib |> apply_right
+    idemp_and <@ true @> |> apply_left |> after_right
+    zero_or <@ true |&| %r @> |> CommuteL |> apply_right
+    implies_true <@ %q |&| %p @> |> Lemma
+]
+
+let ``3.84b`` = theorem prop_calculus <@ %p ==> (%q ==> %p) @> [
+    def_implies |> apply_right
+    def_implies |> apply
+    commute |> apply_left |> after_right |> after_left 
+    distrib |> apply_left
+    left_assoc |> apply_left |> after_left
+    idemp_or p |> apply_left |> after_left |> after_left
+    commute |> apply_left |> after_left 
+    idemp_or p |> apply_right |> after_left
 ] 
-let ``4.1`` = theorem prop_calculus <@ (p ==> q) ==> ((p ||| r) ==> (q ||| r)) @> [
-    def_implies |> R
-    commute_or_or p' r' q' r' |> L |> R'
-    idemp_or r' |> L |> R'
-    commute_or <@ p ||| q @> r' |> L |> R'
-    commute_or q' r' |> R |> R'
-    collect_or_eq r' <@ p ||| q @> q' |> R
-    commute |> R
-    def_implies' p' q' |> Commute |> R
-    weaken_or <@ p ==> q @> r' |> Lemma
-]
-(*
-let ``4.4a`` = theorem prop_calculus <@ p |&| q ==> (p = q) @> [
-    let lemma1 = lemma prop_calculus <@ p |&| q ==> p @> [
-        strengthen_and p' q' |> Lemma
-    ]
-    let lemma2 = lemma prop_calculus <@ p |&| q ==> q @> [
-        commute |> L
-        strengthen_and q' p' |> Lemma
-    ]
-    Deduce lemma1 |> R
-    Deduce lemma2 |> R
-    //def_true <@ true @> |> Commute |> R
-    //implies_true <@ p |&| q @> |> Lemma
+
+let ``4.1`` = theorem prop_calculus <@ (%p ==> %q) ==> ((%p ||| %r) ==> (%q ||| %r)) @> [
+    def_implies |> apply_right
+    commute_or_or p r q r |> apply_left |> after_right
+    idemp_or r |> apply_left |> after_right
+    commute_or <@ %p ||| %q @> r |> apply_left |> after_right
+    commute_or q r |> apply_right |> after_right
+    collect_or_eq r <@ %p ||| %q @> q |> R
+    commute |> apply_right
+    def_implies' p q |> Commute |> apply_right
+    weaken_or <@ %p ==> %q @> r |> Lemma
 ]
 
-let ``4.4b`` = theorem prop_calculus <@ (p ==> r) ==> ((q ==> s) ==> (p |&| q ==> (r |&| s))) @> [
-    shunt' <@ p ==> r @> <@ q ==> s @> <@ (p |&| q ==> (r |&| s)) @> |> Commute |> LR 
-    let lemma1 = lemma prop_calculus <@ (p ==> r) ==> (p = (p |&| r)) @> [
-        commute |> R
-        ident_implies_eq_and_eq p' r' |> L
-        reflex_implies <@ p |&| r = p @> |> Lemma
+let ``4.4a`` = theorem prop_calculus <@ %p |&| %q ==> (%p = %q) @> [
+    let lemma1 = lemma prop_calculus <@ %p |&| %q ==> %p @> [
+        strengthen_and p q |> Lemma
     ]
-    let lemma2 = lemma prop_calculus <@ (q ==> s) ==> (q = (q |&| s)) @> [
-        commute |> R
-        ident_implies_eq_and_eq q' s' |> L
-        reflex_implies <@ q |&| s = q @> |> Lemma
+    let lemma2 = lemma prop_calculus <@ %p |&| %q ==> %q @> [
+        commute |> apply_left
+        strengthen_and q p |> Lemma
     ]
-    Deduce' lemma1 |> R
-    Deduce' lemma2 |> R
-    right_assoc_and p' r' <@ q |&| s @> |> R
-    left_assoc_and r' q' s'|>  R
-    commute_and r' q' |> R
-    right_assoc_and q' r' s' |> R
-    left_assoc_and p' q' <@ r |&| s @> |> R   
-    commute |> L |> R'
-    strengthen_and <@ r |&| s @> <@p |&| q @> |> Taut |> R
+    Deduce lemma1 |> apply_right
+    Deduce lemma2 |> apply_right
+    def_true <@ true @> |> Commute |> apply_right
 ]
-*)
+
+let ``4.4b`` = theorem prop_calculus <@ (%p ==> %r) ==> ((%q ==> %s) ==> (%p |&| %q ==> (%r |&| %s))) @> [
+    shunt' <@ %p ==> %r @> <@ %q ==> %s @> <@ (%p |&| %q ==> (%r |&| %s)) @> |> Commute |> apply
+    let lemma1 = lemma prop_calculus <@ (%p ==> %r) ==> (%p = (%p |&| %r)) @> [
+        commute |> apply_right
+        ident_implies_eq_and_eq p r |> apply_left
+    ]
+    let lemma2 = lemma prop_calculus <@ (%q ==> %s) ==> (%q = (%q |&| %s)) @> [
+        commute |> apply_right
+        ident_implies_eq_and_eq q s |> apply_left
+    ]
+    Deduce' lemma1 |> apply_right
+    Deduce' lemma2 |> apply_right
+    right_assoc_and p r <@ %q |&| %s @> |> apply_right
+    left_assoc_and r q s|>  apply_right
+    commute_and r q |> apply_right
+    right_assoc_and q r s |> apply_right
+    left_assoc_and p q <@ %r |&| %s @> |> apply_right   
+    commute |> apply_left |> after_right
+    strengthen_and <@ %r |&| %s @> <@ %p |&| %q @> |> Taut |> apply_right
+]
