1bf513d1363a7a5f9ab3b6e6412afac62c409777
Allister Beharry <allister.beharry@gmail.com>
2022-07-28 21:07:08 -0400

    solve should return option.

--- src/Math/CAS/Sylvester.CAS.Maxima/Algebra.fs
+++ src/Math/CAS/Sylvester.CAS.Maxima/Algebra.fs
@@ -54,22 +54,25 @@ module Algebra =
         sprintf "solve(%s, %s);" (sprint' system) (sprint' v) 
         |> send 
         |> Result.mapError(fun e -> e.Message)
-        |> Result.bind(fun o -> if o = "" then Error "Maxima did not return a solution." else Infix.parseList (o.Split('=').[1]))
+        |> Result.bind(fun o -> if o = "" then Error "" else Infix.parseList (o.Split('=').[1]))
         |> Result.map(fun e -> e.[0] |> (MathNetExpr.toQuotation<'t> (get_vars system))) 
         |> function
-        | Ok s -> s
+        | Ok s -> Some s
+        | Error "" -> None
         | Error e -> failwithf "Error executing Maxima solve command: %s.\n. Session output:%s." e (Maxima.defaultInt.Value.ConsoleSession.Last10Output)
 
-    let solve_as_func_of (x:Expr<'b>) (v:Expr<'a>) (system:Expr<bool list>) = system |> solve v |> as_func_of x
+    let solve_as_func_of (x:Expr<'b>) (v:Expr<'a>) (system:Expr<bool list>) = system |> solve v |> Option.get |> as_func_of x
 
     let solve2 (x:Expr<'t>) (y:Expr<'t>) (system:Expr<bool list>) =
         sprintf "solve(%s, [%s, %s]);" (sprint' system) (sprint' x) (sprint' y) 
         |> send 
         |> Result.mapError(fun e -> e.Message)
+        |> Result.bind(fun e -> if e = "" then Error "" else Ok e)
         |> Result.map (fun e -> e.TrimStart('[').TrimEnd(']').Split(',') |> Array.map(fun v -> v.Split('=').[1])|> Array.reduce(fun r1 r2 -> r1 + "," + r2))
         |> Result.bind(fun r -> "[" + r + "]" |> MathNet.Symbolics.Infix.parseList)
         |> Result.map(fun e -> e |> List.map (MathNetExpr.toQuotation<'t> (get_vars system))) 
         |> function
-        | Ok (r1::r2::[]) -> r1, r2
-        | Ok _ -> failwithf ""
+        | Ok (r1::r2::[]) -> Some(r1, r2)
+        | Error "" -> None
+        | Ok r -> failwithf "Error executing Maxima solve command: received unexpected solution output: %A." r
         | Error e -> failwithf "Error executing Maxima solve command: %s.\n. Session output:%s." e (Maxima.defaultInt.Value.ConsoleSession.Last10Output)
