26877089cd4b07b56ae60d3f1b27e310de236cbb
Allister Beharry <allister.beharry@gmail.com>
2022-07-31 20:39:08 -0400

    Can use dynamic operator in notebook.

---
+++ notebooks/DataInclude.fsx
@@ -0,0 +1,11 @@
+#r @"C:\Users\Allister\.nuget\packages\microsoft.csharp\4.5.0\lib\netstandard2.0\Microsoft.CSharp.dll"
+#r @"C:\Users\Allister\.nuget\packages\system.dynamic.runtime\4.3.0\lib\netstandard1.3\System.Dynamic.Runtime.dll"
+#r @"C:\Users\Allister\.nuget\packages\system.reflection.emit.lightweight\4.3.0\lib\netstandard1.3\System.Reflection.Emit.Lightweight.dll"
+#r @"C:\Users\Allister\.nuget\packages\csvhelper\12.1.2\lib\netstandard2.0\CsvHelper.dll"
+
+#r "C:\\Projects\\Sylvester.git\\src\\Base\\Sylvester.Data\\bin\\Debug\\netstandard2.0\\Sylvester.Data.dll"
+#r "C:\\Projects\\Sylvester.git\\src\\Data\\Sylvester.DataFrame\\bin\\Debug\\netstandard2.0\\Sylvester.DataFrame.dll"
+
+#load "Paket.fsx"
+Paket.Package [ "FSharp.Interop.Dynamic" ]
+
