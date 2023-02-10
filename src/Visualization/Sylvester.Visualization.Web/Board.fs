21c1f328e332a760bf5b7edddf8f3af5bdd95a67
Allister Beharry <allister.beharry@gmail.com>
2023-02-10 19:16:15 -0400

    Add initial version of JSX type defs from TS import.

---
+++ src/Visualization/Sylvester.Visualization.Web/Board.fs
@@ -0,0 +1,8 @@
+﻿namespace Sylvester
+
+type Board(container:string, renderer:string, id:string, origin: {| usrCoords: float * float * float; scrCoords: float * float * float |}, zoomX, zoomY, unitX, unitY, canvasWidth, canvasHeight, attributes) =
+    member val Container = container
+    member val Renderer = renderer
+    member val Id = id
+    member val Origin = origin
+    
