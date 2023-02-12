b7566832881618806111d4f1a2ded849f4e0116e
Allister Beharry <allister.beharry@gmail.com>
2023-02-12 12:04:36 -0400

    Add create_board function.

--- src/Visualization/Sylvester.Visualization.Web/Board.fs
+++ src/Visualization/Sylvester.Visualization.Web/Board.fs
@@ -9,13 +9,16 @@ open FunScript.Bindings
 open FunScript.Bindings.JSXGraph
 open Html
 
-[<RequireQualifiedAccess>]
+[<AutoOpen>]
 module Board =
-    let draw (src:Expr) =
+    let draw_board (src:Expr) =
         let id = Guid.NewGuid().ToString()
         let repbid (s:string) = s.Replace("_bid", id)
         div [
-            div [attr "id" id; attr "class" "jxgbox"; attr "style" $"width:1080px;height:200px"]
+            div [attr "id" id; attr "class" "jxgbox"; attr "style" $"width:768px;height:200px"]
             script [src |> compile |> repbid |> Text]
-        ] |> Html.toString
+        ]
+        
+    [<Emit("JXG.JSXGraph.initBoard(\"_bid\", {0})")>]
+    let create_board (attr:obj) = stub<Board>
         
