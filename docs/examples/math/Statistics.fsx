3c3fc6fd3e5aa96e568a4d0ba5308bf9c5c1105b
Allister Beharry <allister.beharry@gmail.com>
2025-03-18 22:23:30 -0400

    Add LR functions and abstract members.

--- docs/examples/math/Statistics.fsx
+++ docs/examples/math/Statistics.fsx
@@ -8,7 +8,7 @@ open LinearRegression
 let y,x = realvar2 "y" "x"
 let b0,b1,b2 = realconst3 "beta_0" "beta_1" "beta_2"
 
-let lm = slrm (y == b0 + b1 * x) [
+let lm = slrm' (y == b0 + b1 * x) [
     1,3
     2,5
     3,6
@@ -20,19 +20,17 @@ lm
 lm.Rsquared
 
 let EARNINGS,S,EXP = realvar3  "EARNINGS" "S" "EXP"
-let f = 
+let eawe21 = 
     csv_file "C:\\Users\Allister\\Downloads\\EAWE21.csv"
    |> with_all_col_types<float>
 
-let df = 
-    csv_file "C:\\Users\Allister\\Downloads\\EAWE21.csv"
-    |> with_all_col_types<float>
-    |> frame 
+let m1 = eawe21 |> samples ["S"; "EARNINGS"] |> slrm (EARNINGS == b0 + b1 * S)
 
-let mlm = f |> samples ["S"; "EXP"; "EARNINGS"] |> mlrm (EARNINGS == b0 + b1 * S + b2 * EXP)  
+lrymean m1
+//let mlm = eawe21 |> samples ["S"; "EXP"; "EARNINGS"] |> mlrm (EARNINGS == b0 + b1 * S + b2 * EXP)  
 //mlm |> Seq.map fst |> Seq.item 0 |> Seq.toArray
 //let slm = seq {for r in df -> r.["S"], r.["EARNINGS"]} |> slrm (EARNINGS == b0 + b1 * S)
-mlm
+//mlm
 
 
 //lems witht lm = SimpleLinearRegressionModel(y .= b0 + b1 * x + u + b0, [])
--- src/Math/Sylvester.Statistics/LinearRegression.fs
+++ src/Math/Sylvester.Statistics/LinearRegression.fs
@@ -28,15 +28,27 @@ module Data =
         let df = frame f
         let x = cols |> Seq.take (Seq.length cols - 1) |> Seq.toArray |> df.Sel in
         let y = cols |> Seq.last 
-        Seq.zip (seq {for r in x -> seq {for i in 0 .. n - 1 -> r.[i]}}) (seq {for r in df.[y] -> r})//|> Seq.map (fun x -> x |> Seq.take (n - 1), Seq.last x) 
+        Seq.zip (seq {for r in x -> seq {for i in 0 .. n - 1 -> r.[i]}}) (seq {for r in df.[y] -> r})
 
+[<AbstractClass>]
 type LinearRegressionModel(eqn:ScalarVarMap<real>, samples: (real array*real) seq) =
     let rv = eqn.Rhs |> get_real_vars
     do if samples |> Seq.forall (fun (x, _) -> x.Length = rv.Length) |> not then 
         failwithf "The dimension of a row of the sample data (%A) does not match the number of dependent variables in the regression equation (%A)." (samples |> Seq.find(fun (x,_) -> x.Length <> rv.Length)) rv.Length 
     member val Samples = samples |> Seq.toArray
+    member val N = samples |> Seq.length
     member val DependentVariable = eqn.Var
     member val IndependentVariables = rv |> List.toArray
+    abstract Variables:ScalarVar<real> array with get
+    abstract Parameters:ScalarConst<real> array with get
+    abstract RegressionEquation:ScalarEquation<real> with get
+    abstract RegressionCoefficients:real array with get
+    abstract XSamples:real array array with get
+    abstract XMean:real array with get
+    abstract XVariance:real array with get
+    abstract YSamples:real array with get
+    abstract YMean:real with get
+    abstract YVariance:real with get
 
 type SimpleLinearRegressionModel(eqn:ScalarVarMap<real>, data1:obj seq, data2: obj seq) =
     inherit LinearRegressionModel(eqn, Seq.zip (Seq.map (to_real >> Array.singleton) data1) (Seq.map to_real data2))
@@ -55,18 +67,25 @@ type SimpleLinearRegressionModel(eqn:ScalarVarMap<real>, data1:obj seq, data2: o
         | Some [Constant (ValueWithName(_,_,n)); VariableWithName rv.Name _] -> ScalarConst<real> n
         | _ -> failwithf "Cannot determine the slope coefficient parameter symbol."
     let samples = let d = Seq.zip data1 data2 in seq { for x, y in d -> System.Convert.ToDouble x, System.Convert.ToDouble y  } 
-    let xsamples,ysamples = samples |> Seq.map fst, samples |> Seq.map snd
+    let xsamples,ysamples = samples |> Seq.map fst |> Seq.toArray, samples |> Seq.map snd |> Seq.toArray
     let xmean,xvar = xsamples |> Statistics.MeanVariance
     let ymean,yvar = ysamples |> Statistics.MeanVariance
     let a, b = SimpleRegression.Fit samples
     let rf = fun x -> a + b * x
     let rss = samples |> Seq.sumBy(fun (x,y) -> (y - rf x) ** 2.)
     let ess = ysamples |> Seq.sumBy(fun y -> (y - ymean) ** 2.)
-    member val Variables = [|rv; dv|]
-    member val ParameterConsts = [b0; b1]
-    member val RegressionEquation = dv == b0 + b1 * rv 
+    override val Variables = [|rv; dv|]
+    override val Parameters = [|b0; b1|]
+    override val RegressionEquation = dv == a + b * rv 
+    override val RegressionCoefficients = [|a;b|]
+    override val XSamples = [|xsamples|]
+    override val XMean = [|xmean|]
+    override val XVariance = [|xvar|]
+    override val YSamples = ysamples
+    override val YMean = ymean
+    override val YVariance = yvar
+
     member val RegressionFunc = rf 
-    member val Parameters = [a;b]
     member val Rss = rss
     member val Ess = ess
     member val Tss = ess + rss
@@ -97,19 +116,22 @@ type MultipleLinearRegressionModel(eqn:ScalarVarMap<real>, data1: obj[] seq, dat
     let rv = b1 |> List.map fst
     let samples = base.Samples
     let xsamples,ysamples = samples |> Array.map fst |> Array.transpose, samples |> Array.map snd
-    let xmean,xvar = let mv = xsamples |> Array.map Statistics.MeanVariance in Array.map fst mv, Seq.map snd mv
+    let xmean,xvar = let mv = xsamples |> Array.map Statistics.MeanVariance in Array.map fst mv, Array.map snd mv
     let ymean,yvar = ysamples |> Statistics.MeanVariance
     let a = MultipleRegression.QR(samples, true)
     let re = dv == (a |> Array.skip 1 |> Array.mapi (fun i v -> rv.[i] * v) |> Array.reduce (+)) + a.[0]
     let rf (x:float[]) = (x |> Array.mapi (fun i v -> a.[i] * v) |> Array.reduce (+)) + (a.[0])  
-    member val Variables = rv @ [dv] |> List.toArray 
-    member val ParameterConsts = [b0] @ (b1 |> List.map snd) |> List.toArray
-    member val RegressionEquation = re 
-    member val Parameters = a
-    member val XSamples = xsamples
-    member val YSamples = ysamples
-    member val XMean = xmean
-    member val YMean = ymean
+    override val Variables = rv @ [dv] |> List.toArray 
+    override val Parameters = [b0] @ (b1 |> List.map snd) |> List.toArray
+    override val RegressionEquation = re 
+    override val RegressionCoefficients = a
+    override val XSamples = xsamples
+    override val YSamples = ysamples
+    override val XMean = xmean
+    override val XVariance = xvar
+    override val YMean = ymean
+    override val YVariance = yvar
+
     member __.Item([<ParamArray>] (x:real array)) = rf x
     
     override x.ToString() = sprintf "%A: %A" (x.Samples) re
@@ -117,15 +139,40 @@ type MultipleLinearRegressionModel(eqn:ScalarVarMap<real>, data1: obj[] seq, dat
     new (eqn:ScalarEquation<real>, data1:obj[] seq, data2: obj seq) = MultipleLinearRegressionModel(as_var_map eqn, data1, data2)
 
 module LinearRegression =
-    let slrm (eqn:ScalarEquation<real>) (data:seq<_*_>) =
-        let d1, d2 = data |> Seq.map (fst>>box), data |> Seq.map (snd>>box)
+    let slrm (eqn:ScalarEquation<real>) (data:seq<seq<_>*_>) =
+        let d1 = data |> Seq.map fst |> Seq.map (Seq.item 0 >> box) |> Seq.toArray
+        let d2 = data |> Seq.map (snd>>box)
         SimpleLinearRegressionModel(eqn, d1, d2)
 
-    let slreqn (m:SimpleLinearRegressionModel) = m.RegressionEquation
+    let slrm' (eqn:ScalarEquation<real>) (data:seq<_*_>) =
+         let d1, d2 = data |> Seq.map (fst>>box), data |> Seq.map (snd>>box)
+         SimpleLinearRegressionModel(eqn, d1, d2)
 
     let mlrm (eqn:ScalarEquation<real>) (data:seq<seq<_>*_>) =
-           let d1, d2 = data |> Seq.map (fst>> Seq.map box >> Seq.toArray), data |> Seq.map (snd>>box)
-           MultipleLinearRegressionModel(eqn, d1, d2)
+        let d1, d2 = data |> Seq.map (fst>> Seq.map box >> Seq.toArray), data |> Seq.map (snd>>box)
+        MultipleLinearRegressionModel(eqn, d1, d2)
 
-    let mlreqn (m:MultipleLinearRegressionModel) = m.RegressionEquation
+    let lrvars (m:LinearRegressionModel) = m.Variables
 
+    let lrsamp (m:LinearRegressionModel) = m.Samples
+    
+    let lrN (m:LinearRegressionModel) = m.N
+
+    let lrdvar (m:LinearRegressionModel) = m.DependentVariable
+
+    let lrivars (m:LinearRegressionModel) = m.IndependentVariables
+
+    let lrparams (m:LinearRegressionModel) = m.Parameters
+
+    let lreqn (m:LinearRegressionModel) = m.RegressionEquation
+
+    let lrcoeffs (m:LinearRegressionModel) = m.RegressionCoefficients
+
+    let lrxsamp (m:LinearRegressionModel) = m.XSamples
+
+    let lrysamp (m:LinearRegressionModel) = m.YSamples
+
+    let lrxmean (m:LinearRegressionModel) = m.XMean
+
+    let lrymean (m:LinearRegressionModel) = m.YMean
+   
